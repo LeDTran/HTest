200ceed13da3880179c9b959edd514cc5a470150
le <mrletran99@yahoo.com>
2017-04-20 19:18:05 -0700

    done with position preference

--- scripts/main.js
+++ scripts/main.js
@@ -29,35 +29,8 @@ var app = angular.module('app', []);
 
 //controllers
 app.controller('lineupController', function($scope) {
-  var Player = function(name)
-  {   this.name = name;
-      this.psyhp = 100;
-      this.coghp = 200;
-      this.physhp = 300;
-      this.inningPitch = 'None';
 
-      //Prefer
-      this.preferPi = true;
-      this.preferCa = false;
-      this.prefer1B = false;
-      this.prefer2B = false;
-      this.prefer3B = false;
-      this.preferSS = false;
-      this.preferLF = false;
-      this.preferCF = false;
-      this.preferRF = false;
-
-      //Avoid
-      this.avoidPi = false;
-      this.avoidCa = false;
-      this.avoid1B = false;
-      this.avoid2B = false;
-      this.avoid3B = false;
-      this.avoidSS = false;
-      this.avoidLF = false;
-      this.avoidCF = false;
-      this.avoidRF = false;
-  }
+  console.log('inside main.js');
 
 
   // initial items
--- scripts/player.js
+++ scripts/player.js
@@ -1,6 +1,16 @@
-var Player = function(name, val)
+var Player = function(name)
 {   this.name = name;
-    this.psyhp = val;
-    this.coghp = val;
-    this.physhp = val;
+    this.psyhp = 100;
+    this.coghp = 200;
+    this.physhp = 300;
+    this.inningPitch = 'None';
+
+    //Preferred positions
+    //DUMMY 0, Pi 1, Ca 2, 1B 3, 2B 4, 3B 5, SS 6, LF 7, CF 8, RF 9    
+    this.preferPos = [false,true,true,true,false,false,false,true,true,true];
+
+    //Avoided positions
+    this.avoidPos = [false,false,false,false,true,true,true,false,false,false];
+
 }
+
